b8a61a00325d4467f4501994af4657da71c5464e
Thuan Pham <phamtanthuan2311@gmail.com>
2022-04-02 08:07:31 +0700

    🌟feature(optimization.sh): install html-minifier

--- optimization.sh
+++ optimization.sh
@@ -1,3 +1,8 @@
 #! /usr/bin/env sh
 
-html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true index.html --output index.html
+if hash html-minifier 2>/dev/null; then
+  html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true index.html --output index.html
+else
+  npm install html-minifier -g
+  html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true index.html --output index.html
+fi
