a6643f5a8ea56dc75fd8ebedf8662fa1c5c144d2
Thuan Pham <phamtanthuan2311@gmail.com>
2022-04-02 08:06:15 +0700

    feat: optimization.sh

---
+++ optimization.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env sh
+
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true index.html --output index.html
